f12fc32b68ba9af71ab1a3cb399fcecfa22ecbfa
utsav <utsavmishraa005@gmail.com>
2026-07-12 16:18:11 +0530

    chore: remove temp SQL files, keep repo clean

--- fix_grants.sql
+++
@@ -1,9 +0,0 @@
--- Full grants for service_role (edge functions + REST API)
-GRANT ALL ON profiles TO service_role;
-GRANT ALL ON vehicles TO service_role;
-GRANT ALL ON drivers TO service_role;
-GRANT ALL ON trips TO service_role;
-GRANT ALL ON maintenance_logs TO service_role;
-GRANT ALL ON fuel_logs TO service_role;
-GRANT ALL ON expenses TO service_role;
-GRANT ALL ON notifications TO service_role;
